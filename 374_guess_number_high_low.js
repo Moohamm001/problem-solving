615146f5d5a2967fc4a72617c6a7ecaf6c398595
Moohamm001 <mooham.00771@gmail.com>
2025-04-25 20:30:02 +0700

    leetcode guess number high, low

---
+++ 374_guess_number_high_low.js
@@ -0,0 +1,20 @@
+var guessNumber = function(n) {
+    var g = guess(n)
+    var l = 0
+    var r = n
+    while(l<r){
+        var mid = (l+r)/2
+        var check = guess(mid)
+        if (check==-1){
+            r = mid
+        }
+        if (check==1){
+            l=mid
+        }
+        if(check==0){
+            return mid
+            break
+        }
+    }
+    // return mid
+};
